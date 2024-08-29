9df28180f1438d3622fb33509006c1d98a2fc4e6
heirakug <heirakug@yahoo.co.jp>
2024-08-29 13:54:01 +0900

    modify iot-data.tsx 20240829

--- app/routes/api/iot-data.tsx
+++ app/routes/api/iot-data.tsx
@@ -3,13 +3,13 @@ import { json } from "@remix-run/node";
 import { createIoTData } from "~/models/iotData.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  const { deviceId, data } = await request.json();
+  const { deviceId, data, userId } = await request.json();
 
-  if (typeof deviceId !== "string" || typeof data !== "string") {
+  if (typeof deviceId !== "string" || typeof data !== "string" || typeof userId !== "string") {
     return json({ error: "Invalid input" }, { status: 400 });
   }
 
-  await createIoTData({ deviceId, data });
+  await createIoTData({ deviceId, data userId});
 
   return json({ success: true });
 };
